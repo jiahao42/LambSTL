7c5e15eb1994243daf40d509750aefa89bf85e7d
James <caterpillarous@yahoo.com>
2016-12-09 15:14:40 +0800

    memory pool && commment

--- __default_alloc_template.cpp
+++ __default_alloc_template.cpp
@@ -46,25 +46,53 @@ public:
 			void* r = refill(ROUND_UP(n));
 			return r;
 		}
-		*my_free_list = result -> free_list_link;
+		*my_free_list = result -> free_list_link;//将result从freelist中取出，返回result
 		return result;
 	}
 	static void deallocate(void* p,size_t n){
 		obj* q = (obj*)p;
 		obj* volatile *my_free_list;
 		
-		if (n > (size_t) __MAX_BYTES){
+		if (n > (size_t) __MAX_BYTES){//如果太大直接调用malloc去分配
 			malloc_alloc::deallocate(p,n);
 			return;
 		}
 		my_free_list = free_list + FREELIST_INDEX(n);
 		
-		q->free_list_link = *my_free_list;
+		q->free_list_link = *my_free_list;//将释放的内存块重新放到freelist中
 		*my_free_list = q;
 	}
 	static void* reallocate(void* p, size_t old_sz, size_t new_sz);
-	
 };
+
+template <bool threads, int inst>
+void* __default_alloc_template<threads, inst>::refill(size_t n){
+	int nobjs = 20;//一次分配20个区块
+	char* chunk = chunk_alloc(n,nobjs);//pass by reference
+	obj* volatile * my_free_list;
+	obj* result;
+	obj* current_obj, *next_obj;
+	int i;
+	
+	if (1 == nobjs){//如果只返回了一个区块，则直接给客户使用即可
+		return chunk;
+	}
+	my_free_list = free_list + FREELIST_INDEX(n);
+	result = (obj*)chunk;
+	*my_free_list = next_obj = (obj*)(chunk + n);
+	
+	for (i = 1;;i++){//将所有申请来的区块都链接到freelist上
+		current_obj = next_obj;
+		next_obj = (obj*)((char*)next_obj + n);
+		if (nobjs - 1 == i){
+			current_obj -> free_list_link = 0;
+			break;
+		}else{
+			current_obj -> free_list_link = next_obj;
+		}
+	}
+	return result;
+}
 
 template <bool threads, int inst>
 char* __default_alloc_template<threads,inst>::start_free = 0;
@@ -80,6 +108,53 @@ __default_alloc_template<threads, inst>::obj* volatile
 __default_alloc_template<threads, inst>::free_list[__NFREELISTS] = {
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 };
+
+template <bool threads, int inst>
+char* __default_alloc_template<threads, inst>::chunk_alloc(size_t size, int& nobjs){
+	char* result;
+	size_t total_bytes = size * nobjs;
+	size_t bytes_left = end_free - start_free;//内存池剩余空间
+	
+	if (bytes_left >= total_bytes){//如果内存池内水量充足
+		result = start_free;
+		start_free += total_bytes;
+		return result;
+	}else if (bytes_left >= size){//如果内存池内至少还有一个区块可提供
+		nobjs = bytes_left / size;
+		total_bytes = size * nobjs;
+		result = start_free;
+		start_free += total_bytes;
+		return result;
+	}else{//如果连一个区块也提供不了
+		size_t bytes_to_get = 2 * total_bytes + ROUND_UP(heap_size >> 4);//两倍的当前所需量 + 附加值
+		if (bytes_left > 0){//如果还有剩余的内存，尽量利用，将其加入free_list
+			obj* volatile * my_free_list = free_list + FREELIST_INDEX(bytes_left);
+			((obj*)start_free) -> free_list_link = *my_free_list;
+			*my_free_list = (obj*)start_free;
+		}
+		start_free = (char*)malloc(bytes_to_get);
+		if (0 == start_free){//若heap空间不足，malloc申请失败
+			int i;
+			obj* volatile * my_free_list, *p;
+			for (i = size; i <= __MAX_BYTES; i += __ALIGN){
+				my_free_list = free_list + FREELIST_INDEX(i);
+				p = *my_free_list;
+				if (0 != p){
+					*my_free_list = p->free_list_link;
+					start_free = (char*)p;
+					end_free = start_free + i;
+					return chunk_alloc(size,nobjs);//递归调用申请内存
+				}
+			}
+			end_free = 0;
+			//转而调用上层内存配置器，调用oom机制，会抛出异常
+			start_free = (char*)malloc_alloc::allocate(bytes_to_get);
+		}
+		heap_size += bytes_to_get;
+		end_free = start_free + bytes_to_get;
+		return chunk_alloc(size,nobjs);//递归调用申请内存
+	}
+}
 
 
 
