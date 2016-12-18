28477d5114faec0e596c1f8792fb8e4bed88a3d6
James <caterpillarous@yahoo.com>
2016-12-18 22:55:33 +0800

    add Sample 2

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ SGI STL
 * Sample1 - Trace the memory
 print how STL works when using Vector
 
+* Sample2 - Fit raw pointer
+passing raw pointer such as int* to the template with partial **template specialization**
 
 ## TODO
 
 * 测试int*等偏特化的情况
-* 学习类外定义偏特化模板的方法
+* 学习类外定义偏特化模板的函数的方法
---
+++ Sample2-Fit_raw_pointer/malloc_alloc.h
@@ -0,0 +1,100 @@
+#ifndef __MALLOC_ALLOC_H_
+#define __MALLOC_ALLOC_H_
+
+#include <new>		/* placement new */
+#include <cstddef>	/* for ptrdiff_t, size_t */
+#include <cstdlib>	/* exit() */
+#include <climits>	/* UINT_MAX */
+#include <iostream> /* cerr */
+
+
+#if 0
+#	include <new>
+#	define __THROW_BAD_ALLOC throw bad_alloc
+#elif	!defined(__THROW_BAD_ALLOC)
+#	include <iostream>
+#	define __THROW_BAD_ALLOC std::cerr<<"out of memory"<<std::endl;	exit(1);
+#endif
+
+/*
+第一级配置器，使用malloc(),free(),realloc()来配置内存
+如果配置失败，则调用private的oom_malloc(),oom_realloc()来反复配置内存
+要注意的是，内存分配失败的处理例程要有客户自行设计并设置
+*/
+
+template <int inst>
+class __malloc_alloc_template{
+private:
+	static void *oom_malloc(size_t);
+	static void *oom_realloc(void*, size_t);
+	static void (* __malloc_alloc_oom_handler)();
+
+public:
+	static void* allocate(size_t n){
+		void* result = malloc(n);
+		if (0 == result)	result = oom_malloc(n);
+		return result;
+	}
+	
+	static void deallocate(void* p, size_t){
+		free(p);
+	}
+	
+	static void* reallocate(void* p, size_t, size_t new_sz){
+		void* result = realloc(p, new_sz);
+		if (0 == result)	result = oom_realloc(p, new_sz);
+		return result;
+	}
+	
+	static void (* set_malloc_handler(void(*f)()))(){
+		void (* old)() = __malloc_alloc_oom_handler;
+		__malloc_alloc_oom_handler = f;
+		return old;
+	}
+};
+
+template<int inst>
+void (* __malloc_alloc_template<inst>::__malloc_alloc_oom_handler)() = 0;
+
+template<int inst>
+void* __malloc_alloc_template<inst>::oom_malloc(size_t n){
+	void (* my_malloc_handler)();
+	void *result;
+	
+	for(;;){
+		my_malloc_handler = __malloc_alloc_oom_handler;
+		if (0 == my_malloc_handler)	{ __THROW_BAD_ALLOC; }
+		(*my_malloc_handler)();
+		result = malloc(n);
+		if (result)	return result;
+	}
+}
+
+template <int inst>
+void* __malloc_alloc_template<inst>::oom_realloc(void* p, size_t n){
+	void (* my_malloc_handler)();
+	void* result;
+	
+	for(;;){
+		my_malloc_handler = __malloc_alloc_oom_handler;
+		if (0 == my_malloc_handler)	{__THROW_BAD_ALLOC;}
+		(*my_malloc_handler)();
+		result = realloc(p,n);
+		if (result)	return result;
+	}
+}
+
+typedef __malloc_alloc_template<0> malloc_alloc;
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
