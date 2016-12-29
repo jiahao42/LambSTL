8ed92460dd1f9ca922c0aeaa6f5738bf8dc34a17
James <caterpillarous@yahoo.com>
2016-12-29 10:31:15 +0800

    deque operator overload added

--- src/deque.h
+++ src/deque.h
@@ -46,6 +46,86 @@ protected:
 protected:
 	map_pointer map;
 	size_type map_size;
+	
+public:
+	void set_node(map_pointer new_node){
+		node = new_node;
+		first = *new_node;
+		last = first + difference_type(buffer_size());
+	}
+	
+	reference operator*() const { return *cur; }
+	pointer operator-> const { return &(operator*()); }
+	difference_type operator- (const self& x) const {
+		return difference_type(buffer_size()) * (node - x.node - 1) + (cur - first) + (x.last - x.cur);
+	}
+	
+	//pre
+	self& operator++(){
+		++cur;
+		if (cur == last){
+			set_node(node + 1);
+			cur = first;
+		}
+		return *this;
+	}
+	
+	self operator++(int) {
+		self tmp = *this;
+		++*this;
+		returm tmp;
+	}
+	
+	self& operator-- () {
+		if (cur == first){
+			set_node(node - 1);
+			cur = first;
+		}
+		--cur;
+		return *this;
+	}
+	
+	self operator-- (int) {
+		self tmp = *this;
+		--*this;
+		return tmp;
+	}
+	
+	self& operator+=(difference_type n){
+		difference_type offset = n + (cur - first);
+		if (offset >= 0 && offset < difference_type(buffer_size()))
+			cur+=n;
+		else{
+			difference_type node_offset = offset > 0 ? offset / difference_type(buffer_size()) : -difference_type((-offset - 1) / buffer_size()) - 1;
+			set_node(node + node_offset);
+			cur = first + (offset - node_offset * difference_type(buffer_size()));
+		}
+		return *this;
+	}
+	
+	self operator+ (difference_type n)	const {
+		self tmp = *this;
+		return tmp += n;
+	}
+	
+	self& operator-=(difference_type n ){
+		return *this += -n;
+	}
+	
+	self operator- (difference_type n ) const {
+		self tmp = *this;
+		return tmp -= n;
+	}
+	
+	reference operator[] (difference_type n) const {
+		return *(*this + n);
+	}
+	
+	bool operator== (const self& x)	const { return cur == x.cur; }
+	bool operator!= (const self& x) const { return !(*this == x); }
+	bool operator< (const self& x) const {
+		return (node == x.node) ? (cur < x.cur) : (node < x.node);
+	}
 
 };
 
