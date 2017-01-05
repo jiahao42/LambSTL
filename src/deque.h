d226395d6e046d759ebfa95527d4483ab64a2328
James <caterpillarous@yahoo.com>
2017-01-05 18:28:40 +0800

    push_back or front

--- src/deque.h
+++ src/deque.h
@@ -44,13 +44,122 @@ public:
 	typedef __deque_iterator<T, T&, T*, BufSiz> iterator;
 	
 protected:
+	typedef simple_alloc<value_type, Alloc> data_allocator;
+	typedef simple_alloc<pointer, Alloc> map_allocator;
+	
 	map_pointer map;
 	size_type map_size;
 	
 	iterator start;
 	iterator finish;
 	
+protected:
+	template <class T, class Alloc, size_t BufSiz>
+	void fill_initialize(size_type n, const value_type& value){
+		create_map_and_node(n);
+		map_pointer cur;
+		__STL_TRY{
+			for (cur = start.node; cur < finish.node; ++cur)
+				uninitialized_fill(*cur, *cur + buffer_size(), value);
+			uninitialized_fill(finish.first, finish.cur, value);
+		}catch (...){
+			//TODO
+		}
+	}
+	
+	template <class T, class Alloc, size_t BufSiz>
+	void create_map_and_node(size_type num_elements){
+		size_type num_nodes = num_elements / buffer_size() + 1;
+		map_size = max(initial_map_size(), num_nodes + 2);
+		map = map_allocator::allocate(map_size);
+		
+		map_pointer nstart = map + (map_size - num_nodes) / 2;
+		map_pointer nfinish = nstart + num_nodes - 1;
+		
+		map_pointer cur;
+		__STL_TRY{
+			for (cur = nstart; cur <= nfinish; ++cur)
+				*cur = allocate_node();
+		}catch (...){
+			//commit or rollback
+		}
+		start.set_node(nstart);
+		finish.set_node(nfinish);
+		start.cur = start.first;
+		finish.cur = finish.first + num_elements % buffer_size();
+	}
+	
+	template <class T, class Alloc, size_t BufSiz>
+	void push_back_aux(const value_type& t) {
+		value_type t_copy = t;
+		reserve_map_at_back();
+		*(finish.node + 1) = allocate_node();
+		__STL_TRY{
+			construct(finish.cur, t_copy);
+			finish.set_node(finish.node + 1);
+			finish.cur = finish.first;
+		}
+		__STL_UNWIND(deallocate_node(*(finish.node + 1)));
+	}
+	
+	template <class T, class Alloc, size_t BufSiz>
+	void push_front_aux(const value_type& t){
+		value_type t_copy = t;
+		reserve_map_at_front();
+		*(start.node - 1) = allocate_node();
+		__STL_TRY{
+			start.set_node(start.node - 1);
+			start.cur = start.last - 1;
+			construct(start.cur, t_copy);
+		}catch (...){
+			// commit or rollback
+			start.set_node(start.node + 1);
+			start.cur = start.first;
+			deallocate_node(*(start.node - 1));
+			throw;
+		}
+	}
+	
+	void reverse_map_at_back(size_type nodes_to_add = 1){
+		if (nodes_to_add + 1 > map_size - (finish.node - map))
+			reallocate_map(nodes_to_add, false);
+	}
+	
+	void reserve_map_at_front (size_type nodes_to_add = 1){
+		if (nodes_to_add > start.node - map)
+			reallocate_map(nodes_to_add, true);
+	}
+	
+	template <class T, class Alloc, size_t BufSiz>
+	void reallocate_map(size_type nodes_to_add, bool add_at_front){
+		size_type old_num_nodes = finish.node - start.node + 1;
+		size_type new_num_nodes = old_num_nodes + nodes_to_add;
+		
+		map_pointer new_nstart;
+		if (map_size > 2 * new_num_nodes){
+			new_nstart = map + (map_size - new_num_nodes) / 2 + (add_at_front ? nodes_to_add : 0);
+			if (new_nstart < start.node)
+				copy(start.node, finish.node + 1, new_nstart);
+			else
+				copy_backward(start.node, finish.node + 1, new_nstart + old_num_nodes);
+		}else{
+			size_type new_map_size = map_size + max(map_size) + max(map_size, nodes_to_add) + 2;
+			map_pointer new_map = map_allocator::allocate(new_map_size);
+			new_nstart = new_map + (new_map_size - new_num_nodes) / 2 + (add_at_front ? nodes_to_add : 0);
+			copy(start.node, finish.node + 1, new_nstart);
+			map_allocator::deallocate(map, map_size);
+			map = new_map;
+			map_size = new_map_size;
+		}
+		
+		start.set_node(new_nstart);
+		finish.set_node(new_nstart + old_num_nodes - 1);
+	}
+
 public:
+	deque(int n, const value_type& value) : start(), finish(), map(0), map_size(0) {
+		fill_initialize(n, value);
+	}
 	iterator begin() { return start; }
 	iterator end() { return finish; }
 	reference operator[] (size_type n) {
@@ -142,6 +251,24 @@ public:
 	bool operator!= (const self& x) const { return !(*this == x); }
 	bool operator< (const self& x) const {
 		return (node == x.node) ? (cur < x.cur) : (node < x.node);
+	}
+	
+	void push_back(const value_type& t){
+		if (finish.cur != finish.last - 1){
+			construct(finish.cur, t);
+			++finish.cur;
+		}else{
+			push_back_aux(t);
+		}
+	}
+	
+	void push_front(const value_type& t){
+		if (start.cur != start.first){
+			construct(start.cur - 1, t);
+			--start.cur;
+		}else{
+			push_front_aux(t);
+		}
 	}
 
 };
