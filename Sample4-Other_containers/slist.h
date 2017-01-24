3f04606c9b068b35b025149f9586a48d4d6336f3
James <caterpillarous@yahoo.com>
2017-01-24 15:21:23 +0800

    slist completed

--- Sample4-Other_containers/slist.h
+++ Sample4-Other_containers/slist.h
@@ -1,6 +1,8 @@
 #ifndef __SLIST_H_
 #define __SLIST_H_
 
+#include "simple_alloc.h"
+
 struct __slist_node_base {
 	__slist_node_base* next;
 };
@@ -72,8 +74,78 @@ struct __slist_iterator : public __slist_iterator_base {
 }
 
 
+template <class T, class Alloc = alloc>
+class Slist {
+public:
+	typedef T value_type;
+	typedef value_type* pointer;
+	typedef const value_type* const_pointer;
+	typedef value_type& reference;
+	typedef const value_type& const_reference;
+	typedef size_t size_type;
+	typedef ptrdiff_t difference_type;
+	
+	typedef __slist_iterator<T, T&, T*> iterator;
+	typedef __slist_iterator<T, const T&, const T*> const_iteartor;
+	
+private:
+	typedef __slist_node<T> list_node;
+	typedef __slist_node_base list_node_base;
+	typedef __slist_iterator_base iterator_base;
+	typedef simple_alloc<list_node, Alloc> list_node_allocator;
+	
+	static list_node* create_node(const value_type& x) {
+		list_node* node = list_node_allocator::allocate();
+		__STL_TRY {
+			construct(&node->data, x);
+			node -> next = 0;
+		}
+		__STL_UNWIND(list_node_allocator::deallocate(node));
+		return node;
+	}
+	
+	static void destroy_node(list_node* node) {
+		destroy(&node -> data);
+		list_node_allocator::deallocate(node);
+	}
+private:
+	list_node_base head;
 
-
+public:
+	Slist() { head.next = 0; }
+	~Slist() { clear(); }
+	
+public:
+	iterator begin() {
+		return iterator((list_node*)head.next); 
+	}
+	iterator end() {
+		return iterator(0);
+	}
+	size_type size() const {
+		return __slist_size(head.next); 
+	}
+	bool empty() const { return head.next == 0; }
+	
+	void swap(Slist& L){
+		list_node_base* tmp = head.next;
+		head.next = L.head.next;
+		L.head.next = tmp;
+	}
+public:
+	reference front() { return ((list_node*)head.next)->data; }
+	void push_front(const value_type& x) {
+		__slist_make_link(&head, create_node(x));
+	}
+	
+	//no push_back
+	
+	void pop_front(){
+		list_node* node = (list_node*) head.next;
+		head.next = node -> next;
+		destroy_node(node);
+	}
+};
 
 
 
