17efe81b56de3c23b4666c9903359c6269d57d3a
James <caterpillarous@yahoo.com>
2017-01-29 22:50:06 +0800

    base_iterator completed

--- Sample5/rb_tree.h
+++ Sample5/rb_tree.h
@@ -28,7 +28,50 @@ template <class Value>
 struct __rb_tree_node : public __rb_tree_node_base {
 	typedef __rb_tree_node<Value>* link_type;
 	Value value_field;
-}
+};
+
+
+struct __rb_tree_base_iterator {
+	typedef __rb_tree_node_base::base_ptr base_ptr;
+	typedef bidirectional_iterator_tag iterator_category;
+	typedef ptrdiff_t difference_type;
+	
+	base_ptr node;
+	
+	void increment() {
+		if (node -> right != 0) {
+			node = node -> right;
+			while (node -> left != 0)
+				node = node -> left;
+		}else {
+			base_ptr y = node -> parent;
+			while (node == y -> right) {
+				node = y;
+				y = y -> parent;
+			}
+			if (node -> right != y)
+				node = y;
+		}
+	}
+	
+	void decrement() {
+		if (node -> color == __rb_tree_red && node -> parent -> parent == node)
+			node = node -> right;
+		else if (node -> left != 0) {
+			base_ptr y = node -> left;
+			while (y -> right != 0)
+				y = y -> right;
+			node = y;
+		}else {
+			base_ptr y = node -> parent;
+			while (node == y -> left) {
+				node = y;
+				y = y -> parent;
+			}
+			node = y;
+		}
+	}
+};
 
 
 
