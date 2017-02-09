3ef844e0ce1747323caff149a39f8560e7d71062
James <caterpillarous@yahoo.com>
2017-02-09 23:15:02 +0800

    rb tree update

--- Sample7-RB Tree/rb_tree.h
+++ Sample7-RB Tree/rb_tree.h
@@ -151,6 +151,36 @@ protected:
 		destroy(&p -> value_field);
 		put_node(p);
 	}
+protected:
+	size_type node_count;
+	link_type header;
+	Compare key_compare;
+	
+	link_type& root() const { return (link_type&)header -> parent; }
+	link_type& leftmost() const { return (link_type&)header -> left; }
+	link_type& rightmost() const { return (link_type&)header -> right; }
+	
+	static link_type& left(link_type x) { return (link_type&)x -> left; }
+	static link_type& right(link_type x) { return (link_type&)x -> right; }
+	static link_type& parent(link_type x) { return (link_type&)x -> parent; }
+	static reference value(link_type x) { return x -> value_field; }
+	static const Key& key(link_type x) { return KeyOfValue()(value(x)); }
+	static color_type& color(link_type x) { return (color_type&)x -> color; }
+	
+	static link_type& left(base_ptr x) { return (link_type&)x -> left; }
+	static link_type& right(base_ptr x) { return (link_type&)x -> right; }
+	static link_type& parent(base_ptr x) { return (link_type&)x -> parent; }
+	static reference value(link_type x) { return (link_type)x -> value_field; }
+	static const Key& key(link_type x) { return KeyOfValue()(value(link_type(x))); }
+	static color_type& color(link_type x) { return (color_type&)link_type(x) -> color; }
+	
+	
+	static link_type minimum(link_type x) {
+		return (link_type) __rb_tree_node_base::minimum(x);
+	}
+	static link_type maximum(link_type x) {
+		return (link_type) __rb_tree_node_base::maximum(x);
+	}
 };
 
 
