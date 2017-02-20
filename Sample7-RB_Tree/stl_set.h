d44fe6c1c265574983d46ee28647a08b6664078b
James <caterpillarous@yahoo.com>
2017-02-20 22:28:19 +0800

    set update

--- Sample7-RB_Tree/stl_set.h
+++ Sample7-RB_Tree/stl_set.h
@@ -39,7 +39,53 @@ public:
 		return *this;
 	}
 	
+	//accessors:
+	key_compare key_comp() const { return t.key_comp(); }
+	value_compare value_comp() const { return t.key_comp(); }
+	iterator begin() const { return t.begin(); }
+	iterator end() const { return t.end(); }
+	reverse_iterator rbegin() const { return t.rbegin(); }
+	reverse_iterator rend() const { t.rend(); }
+	bool empty() const { return t.empty(); }
+	size_type size() const { return t.size(); }
+	size_type max_size() const { return t.max_size(); }
+	void swap(set<Key, Compare, Alloc>& x) { t.swap(x.t); }
 	
+	typedef pair<iterator, bool> pair_iterator_bool;
+	pair<iterator, bool> insert(const value_type& x) {
+		pair<typename rep_type::iterator, bool> p = t.insert_unique(x);
+		return pair<iterator, bool>(p.first, p.second);
+	}
+	
+	iterator insert(iterator position, const value_type& x) {
+		typedef typename rep_type::iterator rep_iterator;
+		return t.insert_unique((rep_iterator&)position, x);
+	}
+	
+	template <class InputIterator>
+	void insert(InputIterator first, InputIterator last) {
+		t.insert_unique(first, last);
+	}
+	
+	void erase(iterator position) {
+		typedef typename rep_type::iterator rep_iterator;
+		t.erase((rep_iterator&)position);
+	}
+	
+	size_type erase(const key_type& x) {
+		return t.erase(x);
+	}
+	
+	void erase(iterator first, iterator last) {
+		typedef typename rep_type::iterator rep_iterator;
+		t.erase((rep_iterator&)first, (rep_iterator&)last);
+	}
+	
+	void clear() { t.clear(); }
+	
+	//operations
+	iterator find(const key_type& x) const { return t.find(x); }
+	size_type count(const key_type&)
 	
 };
 
