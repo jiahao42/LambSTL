6f0ca1f8109d84348815f559c61d5d72956fb34f
James <caterpillarous@yahoo.com>
2017-02-20 13:24:35 +0800

    reorganized much leaner now

--- Sample5-Other_containers/src/count_line.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find . | grep -E '^*(\.h|\.cpp)$' | xargs wc -l
--- include/construct.h
+++ include/construct.h
@@ -8,6 +8,11 @@ inline void construct(T1* p, const T2& value){//placement new
 	new (p) T1 (value);
 }
 
+template <class T>
+inline void destroy(T* pointer){
+	pointer->~T();
+}
+
 template <class ForwardIterator>
 inline void __destory_aux(ForwardIterator first, ForwardIterator last, __false_type){
 	for(; first < last; ++first){
@@ -25,17 +30,10 @@ inline void __destory(ForwardIterator first, ForwardIterator last, T*){
 	__destory_aux(first, last, trivial_destructor());
 }
 
-template <class T>
-inline void destroy(T* pointer){
-	pointer->~T();
-}
-
 template <class ForwardIterator>
 inline void destroy(ForwardIterator first, ForwardIterator last){
 	__destory(first, last, value_type(first));
 }
-
-
 
 //以下为destory()针对迭代器为char*与wchar_t*的特化版
 inline void destroy(char*, char*){}
