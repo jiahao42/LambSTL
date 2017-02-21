05e891ce0c4c342852a171bdad4c39f0943d98ee
James <caterpillarous@yahoo.com>
2017-02-21 22:07:46 +0800

    update

--- include/stl_function.h
+++ include/stl_function.h
@@ -93,4 +93,28 @@ struct logical_not : public unary_function<T, bool> {
 	bool operator() (const T& x) const { return !x; }
 };
 
+template <class T>
+struct identity : public unary_function<T, T> {
+	const T& operator() (const T& x) { return x; }
+};
+
+template <class Pair>
+struct select1st : public unary_function<Pair, typename Pair::first_type> {
+	const typename Pair::first_type& operator() (const Pair& x) const { return x.first; }
+};
+
+template <class Pair>
+struct select2nd : public unary_function<Pair, typename Pair::second_type> {
+	const typename Pair::second_type& operator() (const Pair& x) const { return x.second; }
+};
+
+template <class Arg1, class Arg2>
+struct project1st : public binary_function<Arg1, Arg2, Arg1> {
+	Arg1 operator() (const Arg1& x, const Arg2&) const { return x; }
+};
+
+template <class Arg1, class Arg2>
+struct project2nd : public binary_function<Arg1, Arg2, Arg2> {
+	Arg2 operator() (const Arg1&, const Arg2& y) const { return y; }
+};
 # endif
