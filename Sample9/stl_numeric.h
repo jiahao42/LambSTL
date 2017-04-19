f54f2826cf9994d2d3b13461fa7a7fe964688faa
James <caterpillarous@gmail.com>
2017-04-19 11:43:50 +0800

    add accmulate and adjacent_difference

--- Sample9/stl_numeric.h
+++ Sample9/stl_numeric.h
@@ -15,6 +15,42 @@ T accumulate (InputIterator first, InputIterator last, T init, BinaryOperation b
 	return init;
 }
 
+template <class InputIterator, class OutputIterator>
+OutputIterator adjacent_difference(InputIterator first, InputIterator last, OutputIterator result) {
+	if (first == last)	return result;
+	*result = *first;
+	return __adjacent_difference(first, last, result, value_type(first));
+}
+
+template <class InputIterator, class OutputIterator, class T>
+OutputIterator __adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, T*) {
+	T value = *first;
+	while (++first != last) {
+		T tmp = *first;
+		*++result = tmp - value;
+		value = tmp;
+	}
+	return ++result;
+}
+
+template <class InputIterator, class OutputIterator, class BinaryOperation>
+OutputIterator adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, BinaryOperation binary_op) {
+	if (first == last)
+		return result;
+	*result = *first;
+	return __adjacent_difference(first, last, result, value_type(first), binary_op);
+}
+
+template <class InputIterator, class OutputIterator, class T, class BinaryOperation>
+OutputIterator __adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, T*, BinaryOperation binary_op) {
+	T value = *first;
+	while (++first != last) {
+		T tmp = *first;
+		*++result = binary_op(tmp, value);
+		value = tmp;
+	}
+	return ++result;
+}
 
 
 
